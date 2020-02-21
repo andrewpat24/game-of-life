5f0587fdd37de77d548f6c007c07639a5792cbe1
Andrew Patterson <apat24@mail.sfsu.edu>
2020-02-21 15:14:02 -0800

    updates cell count to be 100x100. Makes flipCellValue async. Updates surroundingCell array. Removes debug statements and unnecessary comments.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component, useRef } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    const numRows = 50;
-    const numColumns = 50;
+    const numRows = 100;
+    const numColumns = 100;
 
     this.state = {
       numRows,
@@ -24,10 +24,10 @@ class App extends Component {
     return rows;
   }
 
-  flipCellGridValue(row, col) {
+  async flipCellGridValue(row, col) {
     const newGrid = this.state.grid;
     newGrid[row][col] = newGrid[row][col] === 0 ? 1 : 0;
-    this.setState({
+    return await this.setState({
       grid: newGrid
     });
   }
@@ -70,51 +70,38 @@ class App extends Component {
   }
 
   surroundingCells = [
-    [0, -1],
     [0, 1],
-    [1, 0],
-    [-1, 0],
+    [0, -1],
+    [1, -1],
+    [-1, 1],
     [1, 1],
     [-1, -1],
-    [1, -1],
-    [-1, 1]
+    [1, 0],
+    [-1, 0]
   ];
 
   computeNeighbors(row, col) {
-    const grid = this.state.grid;
-
-    if (grid[(row, col)] === 1) debugger;
     let neighbors = 0;
+
     for (let ii = 0; ii < this.surroundingCells.length; ii++) {
       let neighborVal = 0;
+      const checkRow = row + this.surroundingCells[ii][0];
+      const checkCol = col + this.surroundingCells[ii][1];
 
       // if current cell is on an edge a neighboring value may be undefined.
       if (
-        row + this.surroundingCells[ii][0] >= 0 &&
-        row + this.surroundingCells[ii][0] < this.state.numRows &&
-        col + this.surroundingCells[ii][1] >= 0 &&
-        col + this.surroundingCells[ii][1] < this.state.numColumns
+        checkRow >= 0 &&
+        checkRow < this.state.numRows &&
+        checkCol >= 0 &&
+        checkCol < this.state.numColumns
       )
-        neighborVal =
-          grid[row + this.surroundingCells[ii][0]][
-            col + this.surroundingCells[ii][1]
-          ];
+        neighborVal = this.state.grid[checkRow][checkCol];
 
       neighbors += neighborVal;
     }
 
     return neighbors;
   }
-
-  // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-  // Any live cell with two or three live neighbours lives on to the next generation.
-  // Any live cell with more than three live neighbours dies, as if by overpopulation.
-  // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-  // These rules, which compare the behavior of the automaton to real life, can be condensed into the following:
-
-  // Any live cell with two or three neighbors survives.
-  // Any dead cell with three live neighbors becomes a live cell.
-  // All other live cells die in the next generation. Similarly, all other dead cells stay dead.
 
   runSimulation() {
     const _this = this;
@@ -123,24 +110,26 @@ class App extends Component {
       console.log('_PROCESS ENDED_');
       return;
     }
-
-    console.log(_this.state.running);
-
     for (let row = 0; row < _this.state.numRows; row++) {
       for (let col = 0; col < _this.state.numColumns; col++) {
         let neighbors = this.computeNeighbors(row, col);
         const isAlive = this.state.grid[row][col] === 1 ? true : false;
+
         if (isAlive && (neighbors < 2 || neighbors > 3)) {
-          this.flipCellGridValue(row, col);
+          this.flipCellGridValue(row, col).then(() => {
+            setTimeout(() => {
+              this.runSimulation(this);
+            }, 500);
+          });
         } else if (!isAlive && neighbors === 3) {
-          this.flipCellGridValue(row, col);
+          this.flipCellGridValue(row, col).then(() => {
+            setTimeout(() => {
+              this.runSimulation(this);
+            }, 500);
+          });
         }
       }
     }
-
-    setTimeout(() => {
-      this.runSimulation(this);
-    }, 1000);
   }
 
   render() {
@@ -153,7 +142,6 @@ class App extends Component {
                 running: !this.state.running
               },
               () => {
-                console.log(this.state.running);
                 this.runSimulation();
               }
             );
